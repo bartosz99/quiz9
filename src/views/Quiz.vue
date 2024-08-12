a7572b45dbb37f7d79a072f89935654b373eb82d
bartosz99 <62900319+bartosz99@users.noreply.github.com>
2024-08-12 23:56:21 +0200

    fetch data and implement store

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,1 +1,16 @@
-<template>Hej</template>
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import { useQuizStore } from '@/store/index';
+
+const quizStore = useQuizStore();
+
+const handleStartTest = () => {
+  quizStore.getQuiz();
+};
+</script>
+
+<template>
+  <div class="container max-w-2xl py-24 sm:py-32 flex items-center justify-center">
+    <Button @click="handleStartTest">Start a test</Button>
+  </div>
+</template>
